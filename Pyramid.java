2d9064096d829f169c7822c63e37d01b6c153aa7
Vishal101001 <126702524+Vishal101001@users.noreply.github.com>
2024-09-10 15:40:38 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,27 @@
+package com.pattern;
+//import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		
+			
+				Scanner sc=new Scanner(System.in);
+				int n=sc.nextInt();
+				for(int i=1;i<=n;i++) {
+					for(int k=1;k<=n-i;k++) {
+						System.out.print(" ");
+					}
+					for(int j=1 ;j<=i; j++) {
+						System.out.print(i+" ");
+					}
+					System.out.println();
+				}
+		
+		
+		
+			}
+}
+
